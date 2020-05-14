b20d5ec9a9ba459d4ef1d392688362a913029088
alexsandar <alexsandartenev@gmail.com>
2020-05-14 15:29:51 +0300

    First C# program

---
+++ Project/Test/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+
+            Console.WriteLine(a);
+        }
+    }
+}
