293a0bfe56f1887478cf6c8389fb9c38d8af10dc
imp111 <alexsandartenev@gmail.com>
2022-03-24 16:08:36 +0200

    07.Hot potato added

--- CSharp-Advanced/01. StacksAndQueues - Lab/07. Hot Potato/Program.cs
+++ CSharp-Advanced/01. StacksAndQueues - Lab/07. Hot Potato/Program.cs
@@ -10,6 +10,35 @@ namespace _07.Hot_Potato
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine().Split().ToArray();
+            int numToRemove = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            int i = 0;
+
+            for (i = 0; i < input.Length; i++)
+            {
+                queue.Enqueue(input[i]);
+            }
+
+            i = 1;
+            while (queue.Count > 1)
+            {
+                string personToCheck = queue.Dequeue();
+
+                if (i == numToRemove)
+                {
+                    Console.WriteLine($"Removed {personToCheck}");
+                    i = 0;
+                }
+                else
+                {
+                    queue.Enqueue(personToCheck);
+                }
+
+                i++;
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
         }
     }
 }
