1a7e0468401289ef10635ebfcc55597e2a64d63e
imp111 <alexsandartenev@gmail.com>
2022-03-24 19:09:14 +0200

    08. Traffic Jam added

--- CSharp-Advanced/01. StacksAndQueues - Lab/08. Traffic Jam/Program.cs
+++ CSharp-Advanced/01. StacksAndQueues - Lab/08. Traffic Jam/Program.cs
@@ -10,6 +10,40 @@ namespace _08.Traffic_Jam
     {
         static void Main(string[] args)
         {
+            int numOfCarsToPass = int.Parse(Console.ReadLine());
+            string command = "";
+            Queue<string> queue = new Queue<string>();
+            int counter = 0;
+            while (command.ToLower() != "end")
+            {
+                command = Console.ReadLine();
+
+                if (command == "green")
+                {
+                    for (int i = 0; i < numOfCarsToPass; i++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            counter++;
+                        }
+                    }
+                }
+                else if (command == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
         }
     }
 }
