5e057629cdef78d6e22925fdb05ce2619bdf230c
imp111 <alexsandartenev@gmail.com>
2022-03-25 12:50:56 +0200

    01. Basic Stack Operations added

---
+++ CSharp-Advanced/02. StacksAndQueues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Basic_Queue_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- CSharp-Advanced/02. StacksAndQueues - Exercise/02. StacksAndQueues - Exercise/Program.cs
+++ CSharp-Advanced/02. StacksAndQueues - Exercise/02. StacksAndQueues - Exercise/Program.cs
@@ -10,7 +10,55 @@ namespace _02.StacksAndQueues___Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("asd");
+            int[] parameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = parameters[0]; // number of elements to push into the stack
+            int s = parameters[1]; // number of elements to pop from the stack
+            int x = parameters[2]; // number to look for in the stack
+
+            int[] input = new int[n];
+            input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
+            bool boolOutput = false;
+            int numOutput = 0;
+
+            for (int i = 0; i < n; i++) 
+            {
+                stack.Push(input[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            for (int i = 0; i < stack.Count; i++)
+            {
+                if (stack.Contains(x))
+                {
+                    boolOutput = true;
+                }
+                else if (stack.Count == 0)
+                {
+                    numOutput = 0;
+                }
+                else
+                {
+                    numOutput = stack.Min();
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(stack.Count);
+            }
+            else if (boolOutput == true)
+            {
+                Console.WriteLine("true");
+            }
+            else if (boolOutput == false)
+            {
+                Console.WriteLine(numOutput);
+            }
         }
     }
 }
