3fda746f3d04ba4bb00f024c1155c0ccaf8ae1d9
imp111 <alexsandartenev@gmail.com>
2022-03-27 02:08:25 +0200

    03. Maximum and minimum element added

---
+++ CSharp-Advanced/02. StacksAndQueues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            int[] input = new int[n];
+            // 1 - push
+            // 2 - delete (pop)
+            // 3 - max()
+            // 4 - min()
+            // print in the follwing format n1, n2, n3, ..., nn
+
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int first = input[0];
+                int? second = null;
+
+                if (input.Length < 2)
+                {
+                    continue;
+                }
+                else
+                {
+                    second = input[1];
+                }
+
+                switch (first)
+                {
+                    case 1:
+                        stack.Push(Convert.ToInt32(second));
+                        break; 
+
+                    case 2:
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            stack.Pop();
+                        }
+                        break;
+
+                    case 3:
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine(stack.Max());
+                        }
+                        break;
+
+                    case 4:
+                        if (stack.Count == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine(stack.Min());
+                        }
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+
+            for (int i = 0; i < stack.Count; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
