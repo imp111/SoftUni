a7f4e3812bf02c700a3da706eb391f4fe77cd169
imp111 <alexsandartenev@gmail.com>
2022-03-31 00:01:52 +0300

    05. Fashion Boutique - version1

---
+++ CSharp-Advanced/02. StacksAndQueues - Exercise/05. Fashion Boutique/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Fashion_Boutique
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] clothesInBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacityRack = int.Parse(Console.ReadLine());
+        }
+    }
+}
