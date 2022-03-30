dd16bbaa781bf4dd85845c1b182aac13e53752e7
imp111 <alexsandartenev@gmail.com>
2022-03-31 01:40:19 +0300

    07. Track Tour - added

---
+++ CSharp-Advanced/02. StacksAndQueues - Exercise/07. Track Tour/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Track_Tour
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] petrolValues = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                petrolValues = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+        }
+    }
+}
