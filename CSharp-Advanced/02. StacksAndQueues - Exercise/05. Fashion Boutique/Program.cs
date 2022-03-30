5ecc6726cf38650e399fbc622b18bb26b72be8d0
imp111 <alexsandartenev@gmail.com>
2022-03-31 00:31:21 +0300

    05. Fashion Boutique - finished

--- CSharp-Advanced/02. StacksAndQueues - Exercise/05. Fashion Boutique/Program.cs
+++ CSharp-Advanced/02. StacksAndQueues - Exercise/05. Fashion Boutique/Program.cs
@@ -12,6 +12,35 @@ namespace _05.Fashion_Boutique
         {
             int[] clothesInBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int capacityRack = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < clothesInBox.Length; i++)
+            {
+                stack.Push(clothesInBox[i]);
+            }
+
+            int sum = 0;
+            int numberOfRacks = 0;
+            while (stack.Count != 0)
+            {
+                int checkValue = sum + stack.Peek();
+                if (capacityRack >= checkValue)
+                {
+                    sum += stack.Pop();
+                }
+                else
+                {
+                    numberOfRacks++;
+                    sum = 0;
+                }
+
+                if (stack.Count == 0 && sum != 0)
+                {
+                    numberOfRacks++;
+                }
+            }
+
+            Console.WriteLine(numberOfRacks);
         }
     }
 }
