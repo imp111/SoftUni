28effc58f7c1cdaab8018fd11f7f3ac6cd9b8895
imp111 <alexsandartenev@gmail.com>
2022-03-30 23:31:10 +0300

    04. Fast Food - version1

---
+++ CSharp-Advanced/02. StacksAndQueues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Fast_Food
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int quantityFood = int.Parse(Console.ReadLine());
+            Queue<int> queue = new Queue<int>();
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                queue.Enqueue(input[i]);
+            }
+
+            Console.WriteLine(queue.Max());
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int buffer = 0;
+                buffer = queue.Dequeue();
+
+                if (quantityFood < buffer)
+                {
+                    quantityFood -= buffer;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (quantityFood > 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {quantityFood}");
+            }
+        }
+    }
+}
