baadfb85bb86bc8efcec08c480401a2899ede6e8
imp111 <alexsandartenev@gmail.com>
2022-07-26 14:06:00 +0300

    03. Primary Diagonal added

---
+++ CSharp-Advanced/03. Multidimensional Arrays - Exercises/03. Primary Diagonal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Primary_Diagonal
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[input, input];
+            int[] buffer;
+            int sum = 0;
+            for (int i = 0; i < input; i++)
+            {
+                buffer = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < input; j++)
+                {
+                    matrix[i, j] = buffer[j];
+
+                    if (i == j)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
