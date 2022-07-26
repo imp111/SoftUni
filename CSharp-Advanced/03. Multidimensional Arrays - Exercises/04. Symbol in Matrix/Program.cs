aae4d7c2120e81db0b76d3ccdc26124448df8e1d
imp111 <alexsandartenev@gmail.com>
2022-07-26 14:35:48 +0300

    04. Symbol in Matrix added

---
+++ CSharp-Advanced/03. Multidimensional Arrays - Exercises/04. Symbol in Matrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Symbol_in_Matrix
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[input, input];
+            string buffer;
+
+            for (int i = 0; i < input; i++)
+            {
+                buffer = Console.ReadLine();
+
+                for (int j = 0; j < input; j++)
+                {
+                    matrix[i, j] = buffer[j];
+                }
+            }
+
+            char item = Console.ReadLine()[0];
+            bool check = false;
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = 0; j < input; j++)
+                {
+                    if (item == matrix[i, j])
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        check = true;
+                        break;
+                    }
+                }
+            }
+
+            if (check == false)
+            {
+                Console.WriteLine($"{item} does not occur in the matrix");
+            }
+        }
+    }
+}
