c2b29a6bda32e19ee02825f59cfed1608ecd24fd
imp111 <alexsandartenev@gmail.com>
2022-07-26 13:46:59 +0300

    01. Sum Matrix Elements added

--- CSharp-Advanced/03. Multidimensional Arrays - Exercises/03. Multidimensional Arrays - Exercises/Program.cs
+++ CSharp-Advanced/03. Multidimensional Arrays - Exercises/03. Multidimensional Arrays - Exercises/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace _03.Multidimensional_Arrays___Exercises
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            int[] input = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            int[] buffer;
+            int[,] matrix = new int[input[0], input[1]];
+
+            for (int i = 0; i < input[0]; i++)
+            {
+                buffer = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+                for (int j = 0; j < buffer.Length; j++)
+                {
+                    matrix[i, j] = buffer[j];
+                }
+            }
+            int sum = 0;
+            foreach (var item in matrix)
+            {
+                sum += item;
+            }
+
+            Console.WriteLine(input[0]);
+            Console.WriteLine(input[1]);
+            Console.WriteLine(sum);
         }
     }
 }
