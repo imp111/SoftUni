c175787f147d7b64ecd13115c66525c121e72e26
imp111 <alexsandartenev@gmail.com>
2022-07-26 13:59:58 +0300

    02. Sum Matrix Columns added

--- CSharp-Advanced/03. Multidimensional Arrays - Exercises/02. Sum Matrix Columns/Program.cs
+++ CSharp-Advanced/03. Multidimensional Arrays - Exercises/02. Sum Matrix Columns/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace _02.Sum_Matrix_Columns
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            int[] input = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            int[] buffer;
+            int[] sum = new int[input[1]];
+            int[,] matrix = new int[input[0], input[1]];
+
+            for (int i = 0; i < input[0]; i++) //row
+            {
+                buffer = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < input[1]; j++) //column
+                {
+                    matrix[i, j] = buffer[j];
+                }
+            }
+
+            for (int i = 0; i < input[1]; i++)
+            {
+                for (int j = 0; j < input[0]; j++)
+                {
+                    sum[i] += matrix[j, i];
+                }
+                Console.WriteLine(sum[i]);
+            }
         }
     }
 }
