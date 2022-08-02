330b48cb65da4c568dafbb1ba3d40e1a897bb428
imp111 <alexsandartenev@gmail.com>
2022-08-02 16:01:32 +0300

    01. Person added

---
+++ CSharp-Advanced/03. Multidimensional Arrays - Exercises/07. Pascal Triangle/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Pascal_Triangle
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int[][] jagged = new int[input][];
+        }
+    }
+}
---
+++ CSharp-OOP/01. Inheritance - Exercise/01. Inheritance - Exercise/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Inheritance___Exercise
+{
+    internal class Person
+    {
+        public string Name;
+        public int Age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format($"Name: {this.Name}, Age: {this.Age}"));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
