6cc9a6d702a0047934e903f861a9f66a25dd9861
imp111 <alexsandartenev@gmail.com>
2022-10-07 14:47:14 +0300

    06. Birthday Celebrations - Done

---
+++ CSharp-OOP/03. Interfaces and Abstraction/06. Birthday Celebrations/Program.cs
@@ -0,0 +1,26 @@
+﻿using _06._Birthday_Celebrations;
+using _06._Birthday_Celebrations.Interfaces;
+
+List<IDate> list = new List<IDate>();
+string command;
+
+while ((command = Console.ReadLine().ToLower()) != "end")
+{
+    var tokens = command.Split();
+
+    if (tokens[0].ToLower() == "citizen")
+    {
+        list.Add(new Person(tokens[1], int.Parse(tokens[2]), tokens[3], tokens[4]));
+    }
+    else if (tokens[0].ToLower() == "pet")
+    {
+        list.Add(new Pet(tokens[1], tokens[2]));
+    }
+}
+
+string fakeIdNum = Console.ReadLine();
+
+list.Where(c => c.Date.EndsWith(fakeIdNum))
+            .Select(c => c.Date)
+            .ToList()
+            .ForEach(Console.WriteLine);
