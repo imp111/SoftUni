f4baad073e501836935f609abb6b3715c3d5e913
imp111 <alexsandartenev@gmail.com>
2022-10-07 12:30:53 +0300

    05. Border Controll - Done

---
+++ CSharp-OOP/03. Interfaces and Abstraction/04. Border Control/Person.cs
@@ -0,0 +1,23 @@
+﻿using _04._Border_Control.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Border_Control
+{
+    internal class Person : IDentity
+    {
+        public string Name { get; private set; }
+        public string Id { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string name, int age, string id)
+        {
+            Name = name;
+            Id = id;
+            Age = age;
+        }   
+    }
+}
--- CSharp-OOP/03. Interfaces and Abstraction/04. Border Control/Program.cs
+++ CSharp-OOP/03. Interfaces and Abstraction/04. Border Control/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using _04._Border_Control;
+using _04._Border_Control.Interfaces;
+
+List<IDentity> list = new List<IDentity>();
+string command;
+
+while ((command = Console.ReadLine().ToLower()) != "end")
+{
+    var tokens = command.Split();
+
+    if (tokens.Length == 3)
+    {
+        list.Add(new Person(tokens[0], int.Parse(tokens[1]), tokens[2]));
+    }
+    else if (tokens.Length == 2)
+    {
+        list.Add(new Robot(tokens[0], tokens[1]));
+    }
+}
+
+string fakeIdNum =Console.ReadLine();
+
+list.Where(c => c.Id.EndsWith(fakeIdNum))
+            .Select(c => c.Id)
+            .ToList()
+            .ForEach(Console.WriteLine);
