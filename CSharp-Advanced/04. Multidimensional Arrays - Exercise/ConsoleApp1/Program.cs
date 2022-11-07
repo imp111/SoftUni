e7c9877436ab641fd113501dbd77111fd8b5ad34
imp111 <alexsandartenev@gmail.com>
2022-11-07 20:02:41 +0200

    01. Diagonal Difference - done

--- CSharp-Advanced/04. Multidimensional Arrays - Exercise/ConsoleApp1/Program.cs
+++ CSharp-Advanced/04. Multidimensional Arrays - Exercise/ConsoleApp1/Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    for (int j = 0; j < n; j++)
+    {
+        
+        array[i, j] = input[j];
+    }
+}
+
+Console.WriteLine($"{Math.Abs(PrimaryDiagonal(array, n) - SecondaryDiagonal(array, n))}");
+
+
+int PrimaryDiagonal(int[,] array, int n)
+{
+    int primarySum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                primarySum += array[i, j];
+            }
+        }
+    }
+
+    return primarySum;
+}
+
+int SecondaryDiagonal(int[,] array, int n)
+{
+    int secondarySum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ((i + j) == (n - 1))
+            {
+                secondarySum += array[i, j];
+            }
+        }
+    }
+
+    return secondarySum;
+}
+
