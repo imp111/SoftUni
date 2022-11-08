e6a235a60d6b6061fccbdc55404603a5bfaea8f2
imp111 <alexsandartenev@gmail.com>
2022-11-08 12:51:02 +0200

    02. 2x2 squares in matrix added

---
+++ CSharp-Advanced/04. Multidimensional Arrays - Exercise/02. 2x2 Squares in Matrix/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+string[,] array = new string[n[0], n[1]];
+
+for (int i = 0; i < n[0]; i++)
+{
+    string[] input = Console.ReadLine().Split().ToArray();
+
+    for (int j = 0; j < n[1]; j++)
+    {
+        array[i, j] = input[j];
+    }
+}
