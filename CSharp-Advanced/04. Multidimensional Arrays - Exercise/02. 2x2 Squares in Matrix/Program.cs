b83a98300fa5f6960a4f9523f602552dc396e794
imp111 <alexsandartenev@gmail.com>
2022-11-09 12:03:09 +0200

    02. 2x2 Squares in Matrix - done

--- CSharp-Advanced/04. Multidimensional Arrays - Exercise/02. 2x2 Squares in Matrix/Program.cs
+++ CSharp-Advanced/04. Multidimensional Arrays - Exercise/02. 2x2 Squares in Matrix/Program.cs
@@ -11,3 +11,23 @@ for (int i = 0; i < n[0]; i++)
         array[i, j] = input[j];
     }
 }
+
+Console.WriteLine(SquareOfEquals(array, n[0], n[1]));
+
+int SquareOfEquals(string[,] array, int rows, int columns)
+{
+    int count = 0;
+
+    for (int i = 0; i < rows - 1; i++)
+    {
+        for (int j = 0; j < columns - 1; j++)
+        {
+            if (array[i, j] == array[i, j + 1] && array[i, j] == array[i + 1, j] && array[i, j] == array[i + 1, j + 1])
+            {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
