73f412f60c95423cc5a6ac69e3c3813424ae965b
imp111 <alexsandartenev@gmail.com>
2022-11-18 13:24:13 +0200

    04 Matrix Shuffling - done

--- CSharp-Advanced/04. Multidimensional Arrays - Exercise/03. Maximal Sum/Program.cs
+++ CSharp-Advanced/04. Multidimensional Arrays - Exercise/03. Maximal Sum/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int[,] array = new int[dimensions[0], dimensions[1]];
+int[,] outputMatrix = new int[3, 3];
+
+for (int i = 0; i < dimensions[0]; i++)
+{
+    int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    for (int j = 0; j < dimensions[1]; j++)
+    {
+        array[i, j] = input[j];
+    }
+}
+
+Console.WriteLine(MatrixSum(array, dimensions[0], dimensions[1]));
+
+foreach (var item in outputMatrix)
+{
+    Console.WriteLine(item);
+}
+
+int MatrixSum(int[,] array, int rows, int columns)
+{
+    int sum = 0;
+    int maxSum = 0;
+
+    for (int i = 0; i < rows - 2; i++)
+    {
+        for (int j = 0; j < columns - 2; j++)
+        {
+            sum += array[i, j] + array[i, j + 1] + array[i, j + 2] + array[i + 1, j] + array[i + 1, j + 1] + array[i + 1, j + 2] + array[i + 2, j] + array[i + 2, j + 1] + array[i + 2, j + 2];
+            
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+                sum = 0;
+            }
+            else
+            {
+                sum = 0;
+            }
+        }
+    }
+
+    return maxSum;
+}
+
+int Matrix(int[,] outputMatrix, int inputMatrix, int rows, int columns)
+{
+    for (int i = 0; i < rows - 2; i++)
+    {
+        for (int j = 0; j < columns - 2; j++)
+        {
+           // sum += array[i, j] + array[i, j + 1] + array[i, j + 2] + array[i + 1, j] + array[i + 1, j + 1] + array[i + 1, j + 2] + array[i + 2, j] + array[i + 2, j + 1] + array[i + 2, j + 2];
+        }
+    }
+
+    return 0;
+}
---
+++ CSharp-Advanced/04. Multidimensional Arrays - Exercise/04. Matrix Shuffling/Program.cs
@@ -0,0 +1,59 @@
+﻿int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int rows = dimensions[0], columns = dimensions[1];
+string[,] matrix = new string[rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    string[] input = Console.ReadLine().Split().ToArray();
+
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i, j] = input[j];
+    }
+}
+
+string command = Console.ReadLine();
+
+while (command.ToLower() != "end")
+{
+    string[] input = command.Split().ToArray();
+
+    if (input[0].ToLower() != "swap" && input.Length != 5)
+    {
+        Console.WriteLine("Invalid input!");
+    }
+    else
+    {
+        if (int.Parse(input[1]) >= rows || int.Parse(input[3]) >= rows || int.Parse(input[2]) >= columns || int.Parse(input[4]) >= columns)
+        {
+            Console.WriteLine("Invalid input!");
+        }
+        else
+        {
+            string buffer1 = " ";
+            buffer1 = matrix[int.Parse(input[1]), int.Parse(input[2])];
+            matrix[int.Parse(input[1]), int.Parse(input[2])] = matrix[int.Parse(input[3]), int.Parse(input[4])];
+            matrix[int.Parse(input[3]), int.Parse(input[4])] = buffer1;
+
+            PrintMatrix(matrix, rows, columns);
+        }
+    }
+
+    command = Console.ReadLine();
+}
+        
+static void PrintMatrix(string[,] matrix, int rows, int columns)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+
+            if (j == columns - 1)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
