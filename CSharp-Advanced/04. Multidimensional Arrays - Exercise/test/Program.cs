0af839ceb46bc2a7a6121858e0c056e313e87f90
imp111 <alexsandartenev@gmail.com>
2022-11-28 17:52:36 +0200

    Java page renamed:

---
+++ CSharp-Advanced/04. Multidimensional Arrays - Exercise/test/Program.cs
@@ -0,0 +1,36 @@
+﻿using test;
+
+string[] input = Console.ReadLine().Split().ToArray();
+List<Student> students = new List<Student>();
+
+while (input[0].ToLower() != "exit")
+{
+    if (input.Length == 4)
+    {
+        students.Add(new Student(input[1], int.Parse(input[2]), double.Parse(input[3])));
+    }
+    else if (input.Length == 2)
+    {
+        Student searchedStudent = SearchStudent(students, input[1]);
+        searchedStudent.DisplayInformation(searchedStudent);
+        
+    }
+    else
+    {
+
+    }
+}
+
+
+static Student? SearchStudent(List<Student> students, string name)
+{
+    foreach (var student in students)
+    {
+        if (student.Name == name)
+        {
+            return student;
+        }
+    }
+
+    return null;
+}
---
+++ CSharp-Advanced/04. Multidimensional Arrays - Exercise/test/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Student
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+        public string? Message { get; set; }
+
+        public Student(string? name, int age, double grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+
+        public void DisplayInformation(Student student)
+        {
+            if (student.Grade >= 5.50)
+            {
+                student.Message = "Excelent student";
+            }
+            else if (student.Grade >= 3.00)
+            {
+                student.Message = "Average student";
+            }
+            else
+            {
+                student.Message = "Very nice person";
+            }
+
+            Console.WriteLine($"{student.Name} is {student.Age} years old. {student.Message}.");
+        }
+    }
+}
