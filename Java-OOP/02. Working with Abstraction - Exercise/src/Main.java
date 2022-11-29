bdc68b3e9051c6b75c0e386bd7f1609671b0479a
imp111 <alexsandartenev@gmail.com>
2022-11-29 22:03:14 +0200

    jedi galaxy - potentionaly done??

--- Java-OOP/02. Working with Abstraction - Exercise/src/Main.java
+++ Java-OOP/02. Working with Abstraction - Exercise/src/Main.java
@@ -14,40 +14,6 @@ public class Main {
         String command = Input.readString();
 
         long sum = 0;
-
-        while (!command.equalsIgnoreCase("let the force be with you"))
-        {
-            int[] ivoS = Input.readStringToArray(command);
-            int[] evil = Input.dimensionsReader();
-            int xE = evil[0];
-            int yE = evil[1];
-
-            while (xE >= 0 && yE >= 0)
-            {
-                if (xE >= 0 && xE < matrix.length && yE >= 0 && yE < matrix[0].length)
-                {
-                    matrix[xE][yE] = 0;
-                }
-                xE--;
-                yE--;
-            }
-
-            int xI = ivoS[0];
-            int yI = ivoS[1];
-
-            while (xI >= 0 && yI < matrix[1].length)
-            {
-                if (xI >= 0 && xI < matrix.length && yI >= 0 && yI < matrix[0].length)
-                {
-                    sum += matrix[xI][yI];
-                }
-
-                yI++;
-                xI--;
-            }
-
-            command = Input.readString();
-        }
-        System.out.println(sum);
+        System.out.println(Output.mainFunction(command, matrix, sum));
     }
 }
