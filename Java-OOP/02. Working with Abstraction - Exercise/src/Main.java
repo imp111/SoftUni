3759b2443102a4c253a717f8b790620102e1a0cc
imp111 <alexsandartenev@gmail.com>
2022-11-29 09:45:36 +0200

    Traffic Lights - refactored and done

--- Java-OOP/02. Working with Abstraction - Exercise/src/Main.java
+++ Java-OOP/02. Working with Abstraction - Exercise/src/Main.java
@@ -7,18 +7,7 @@ public class Main {
         Integer n = Input.ReadNumberOfOperations();
         List<TrafficLight> lightsList = new ArrayList<>();
 
-        for (int i = 0; i < input.length; i++) {
-            TrafficLight light = new TrafficLight(Colors.valueOf(input[i]));
-            lightsList.add(light);
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (TrafficLight light : lightsList) {
-                light.changeColor();
-                System.out.print(light.getColor() + " ");
-            }
-
-            System.out.println();
-        }
+        Output.AddLightsToList(input, lightsList);
+        Output.PrintLightsList(n, lightsList);
     }
 }
---
+++ Java-OOP/02. Working with Abstraction - Exercise/src/Output.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Output {
+    public static void AddLightsToList(String[] input, List<TrafficLight> lightsList) {
+        for (String s : input) {
+            TrafficLight light = new TrafficLight(Colors.valueOf(s));
+            lightsList.add(light);
+        }
+    }
+
+    public static void PrintLightsList(Integer n, List<TrafficLight> lightsList) {
+        for (int i = 0; i < n; i++) {
+            for (TrafficLight light : lightsList) {
+                light.changeColor();
+                System.out.print(light.getColor() + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
