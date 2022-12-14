ed1c5b845342d8cb9441551f1d935e62058be570
imp111 <alexsandartenev@gmail.com>
2022-12-14 18:03:01 +0200

    01. Edited and Done

--- MYSQL/06. Built-in Functions - Exercise/01. Find Names of All Employees by First Name.sql
+++ MYSQL/06. Built-in Functions - Exercise/01. Find Names of All Employees by First Name.sql
@@ -1,8 +1,10 @@
+#Solution 1
 SELECT `first_name`, `last_name` 
 FROM `employees`
 WHERE `first_name` REGEXP '^Sa *'
 ORDER BY `employee_id`;
 
+# Solution 2
 SELECT `first_name`, `last_name`
 FROM `employees`
 WHERE SUBSTR(`first_name`, 1, 2) = 'Sa'
