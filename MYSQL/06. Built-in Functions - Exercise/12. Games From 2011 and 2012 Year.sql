ca9b4b2e7f1a3b45a7b4d8b2a31f8512f8edaf94
imp111 <alexsandartenev@gmail.com>
2022-12-16 16:36:25 +0200

    12. Games From 2011 and 2012 Year - updated and done

--- MYSQL/06. Built-in Functions - Exercise/12. Games From 2011 and 2012 Year.sql
+++ MYSQL/06. Built-in Functions - Exercise/12. Games From 2011 and 2012 Year.sql
@@ -9,3 +9,15 @@ SELECT `name`, DATE_FORMAT(`start`, '%Y-%m-%d') AS `start` FROM `games`
 WHERE YEAR(`start`) BETWEEN 2011 AND 2012
 ORDER BY `start`, `name`
 LIMIT 50;
+
+# Solution 3
+SELECT `name`, DATE(`start`) AS `start` FROM `games`
+WHERE YEAR(`start`) BETWEEN 2011 AND 2012
+ORDER BY `start`, `name`
+LIMIT 50;
+
+# Solution 4
+SELECT `name`, DATE(`start`) AS `start` FROM `games`
+WHERE YEAR(`start`) IN ('2011', '2012')
+ORDER BY `start`, `name`
+LIMIT 50;
