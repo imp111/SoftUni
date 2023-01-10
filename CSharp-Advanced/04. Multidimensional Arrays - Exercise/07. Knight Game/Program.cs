99e0fceb949fc6446d543943d4f5a352eb150466
imp111 <alexsandartenev@gmail.com>
2023-01-10 16:50:02 +0200

    01. Recursive Array Sum - added and done

---
+++ CSharp-Advanced/04. Multidimensional Arrays - Exercise/07. Knight Game/Program.cs
@@ -0,0 +1,35 @@
+﻿int boardSize = int.Parse(Console.ReadLine());
+char[,] matrix = new char[boardSize, boardSize];
+
+AssignMatrtix(matrix, boardSize);
+PrintMatrix(matrix, boardSize);
+
+static void AssignMatrtix(char[,] matrix, int boardSize)
+{
+    for (int i = 0; i < boardSize; i++)
+    {
+        char[] charArray = Console.ReadLine().ToCharArray();
+
+        if (charArray.Length == boardSize)
+        {
+            for (int j = 0; j < boardSize; j++)
+            {
+                matrix[i, j] = charArray[j];
+            }
+        }   
+    }
+}
+
+static void PrintMatrix(char[,] matrix, int boardSize)
+{
+    Console.WriteLine();
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; j++)
+        {
+            Console.Write($"{matrix[i, j]}"); 
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ CSharp-AlgorithmsFundamentals/01. Recursion, Sorting and Searching Algorithms - Lab/01. Recursion, Sorting and Searching Algorithms/01. Recursion, Sorting and Searching Algorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[] { 1, 2, 3, 4, 5 };
+int sum = Sum(array, 0);
+Console.WriteLine(sum);
+
+static int Sum(int[] array, int index)
+{
+    if (index == array.Length)
+    {
+        return 0;
+    }
+
+    return array[index] + Sum(array, index + 1);
+}
