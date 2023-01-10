f8ba2c04eb71f598ce027817136b563f0f8eec02
imp111 <alexsandartenev@gmail.com>
2023-01-10 17:07:10 +0200

    01. Recursive Array Sum - edited and done

---
+++ CSharp-AlgorithmsFundamentals/01. Recursion, Sorting and Searching Algorithms - Lab/01. Rec, Sort and Search/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Rec__Sort_and_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = Sum(array, 0);
+            Console.WriteLine(sum);
+        }
+
+        public static int Sum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            return array[index] + Sum(array, index + 1);
+        }
+    }
+}
