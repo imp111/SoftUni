4bedbddf0f35d6d47365aff4d36d2966e326c76e
imp111 <alexsandartenev@gmail.com>
2023-01-10 17:14:46 +0200

    02. Recursive Factorial - added and done

--- CSharp-AlgorithmsFundamentals/01. Recursion, Sorting and Searching Algorithms - Lab/02. Recursive Factorial/Program.cs
+++ CSharp-AlgorithmsFundamentals/01. Recursion, Sorting and Searching Algorithms - Lab/02. Recursive Factorial/Program.cs
@@ -10,6 +10,19 @@ namespace _02.Recursive_Factorial
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            long output = Factorial(n);
+            Console.WriteLine(output);
+        }
+
+        public static long Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
         }
     }
 }
