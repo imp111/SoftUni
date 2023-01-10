d40ecb0be2f73b36fa052170e101711c4f8253f2
imp111 <alexsandartenev@gmail.com>
2023-01-10 21:58:26 +0200

    05. Generating Combinations - added and done

---
+++ CSharp-AlgorithmsFundamentals/01. Recursion, Sorting and Searching Algorithms - Lab/05. Generating Combinations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Generating_Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] set = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int generateNumber = int.Parse(Console.ReadLine());
+            int[] vector = new int[generateNumber];
+            GenCombs(set, vector, 0, -1);
+        }
+
+        public static void GenCombs(int[] set, int[] vector, int index, int border) 
+        {
+            if (index >= vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+            }
+            else
+            {
+                for (int i = border + 1; i < set.Length; i++)
+                {
+                    vector[index] = set[i];
+                    GenCombs(set, vector, index + 1, i);
+                }
+            }
+        }
+    }
+}
