b8346ae6d0d01e5f291214e90260b27a8207ab6a
imp111 <alexsandartenev@gmail.com>
2023-03-22 23:26:09 +0200

    rhombus of starts added

--- Java/OOP/Working with Abstraction - Lab/src/RhombusOfStars/Main.java
+++ Java/OOP/Working with Abstraction - Lab/src/RhombusOfStars/Main.java
@@ -1,7 +1,30 @@
 package RhombusOfStars;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        Integer n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            printRow(n, i);
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            printRow(n, i);
+        }
+    }
+
+    public static void printRow(Integer count, Integer starCount) {
+        for (int i = 0; i < count - starCount; i++) {
+            System.out.print(" ");
+        }
+
+        for (int i = 1; i < starCount; i++) {
+            System.out.print("* ");
+        }
+        System.out.println("*");
     }
 }
