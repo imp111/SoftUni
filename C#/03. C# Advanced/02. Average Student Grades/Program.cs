2f3b79d7d17fc8d801087ca6616b8417c9c4bf9c
imp111 <alexsandartenev@gmail.com>
2023-03-23 00:25:20 +0200

    average student grades added

---
+++ C#/03. C# Advanced/02. Average Student Grades/Program.cs
@@ -0,0 +1,39 @@
+﻿int n = int.Parse(Console.ReadLine());
+Dictionary<string, List<decimal>> dictionary = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split().ToArray();
+
+    if (input.Length > 2)
+    {
+        continue;
+    }
+    else
+    {
+        if (dictionary.ContainsKey(input[0]))
+        {
+            dictionary[input[0]].Add(decimal.Parse(input[1]));
+        }
+        else
+        {
+            dictionary.Add(input[0], new List<decimal>());
+            dictionary[input[0]].Add(decimal.Parse(input[1]));
+        }
+    }
+}
+
+foreach (var item in dictionary)
+{
+    Console.Write($"{item.Key} -> ");
+
+    for (int i = 0; i < item.Value.Count; i++)
+    {
+        Console.Write($"{item.Value[i]:F2} ");
+        
+    }
+    decimal average = item.Value.Average(); ;
+    Console.Write($"(avg: {average:F2})");
+    Console.WriteLine(  );
+}
+
