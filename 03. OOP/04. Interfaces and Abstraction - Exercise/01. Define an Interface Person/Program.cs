3185620782fa2e38ed2dc292740f7344803fa403
imp111 <alexsandartenev@gmail.com>
2023-03-27 19:08:30 +0300

    interfaces and abstraction - exercise folder added

---
+++ 03. OOP/04. Interfaces and Abstraction - Exercise/01. Define an Interface Person/Program.cs
@@ -0,0 +1,7 @@
+﻿string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+
+IPerson person = new Citizen(name, age);
+
+Console.WriteLine(person.Name);
+Console.WriteLine(person.Age);
