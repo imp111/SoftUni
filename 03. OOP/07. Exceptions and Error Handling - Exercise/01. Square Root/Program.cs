ce7010456fc74fc53e34c0dd261e35c3c94b4bf7
imp111 <alexsandartenev@gmail.com>
2023-03-31 21:50:26 +0300

    02 and 03 added and done

---
+++ 03. OOP/07. Exceptions and Error Handling - Exercise/01. Square Root/Program.cs
@@ -0,0 +1,26 @@
+﻿
+try
+{
+    int n = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(Sqrt(n));
+}
+catch (ArgumentOutOfRangeException)
+{
+    Console.WriteLine("Invalid number");
+    throw;
+}
+finally
+{
+    Console.WriteLine("Goodbye");
+}
+
+static double Sqrt(double value)
+{
+    if (value < 0)
+    {
+        throw new ArgumentOutOfRangeException(nameof(value), "Sqrt for negative numbers is undefined!");
+    }
+
+    return Math.Sqrt(value);
+}
