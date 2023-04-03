f7ef2d7a791347bd9f1447701d98b370aa607d24
imp111 <alexsandartenev@gmail.com>
2023-04-03 21:21:45 +0300

    valid person - done, custom exception - added

---
+++ 03. OOP/07. Exceptions and Error Handling - Exercise/06. Valid Person/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._Valid_Person
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The first name cannot be null or empty string");
+                }
+                else
+                {
+                    firstName = value;
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "The first name cannot be null or empty string");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age should be in the range [0...120]");
+                }
+
+                age = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+    }
+}
---
+++ 03. OOP/07. Exceptions and Error Handling - Exercise/07. Custom Exception/Program.cs
@@ -0,0 +1,25 @@
+﻿using _07._Custom_Exception;
+
+try
+{
+    string[] input = Console.ReadLine().Split().ToArray();
+
+    if (input.Length != 3)
+    {
+        throw new ArgumentOutOfRangeException(nameof(input), "There should be 3 parameters");
+    }
+
+    string firstName = input[0];
+    string lastName = input[1];
+    int age = int.Parse(input[2]);
+
+    Person person = new Person(firstName, lastName, age);
+}
+catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine(e.Message);
+}
