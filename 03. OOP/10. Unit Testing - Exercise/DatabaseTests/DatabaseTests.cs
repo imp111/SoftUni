366ac987814a0b11a759f8ddb46279169141fdcf
imp111 <alexsandartenev@gmail.com>
2023-04-06 22:26:58 +0300

    database tests done

--- 03. OOP/10. Unit Testing - Exercise/DatabaseTests/DatabaseTests.cs
+++ 03. OOP/10. Unit Testing - Exercise/DatabaseTests/DatabaseTests.cs
@@ -1,10 +1,11 @@
 using _01._Database;
+using System.Runtime.CompilerServices;
 
 namespace DatabaseTests
 {
     public class DatabaseTests
     {
-        private readonly int[] data = new int[16];
+        private readonly int[] data = { 1, 2, 3, 4, 5, 6, 7, 8 };
         private Database database;
         public DatabaseTests()
         {
@@ -35,7 +36,8 @@ namespace DatabaseTests
         [Fact]
         public void AddElementToDatabase()
         {
-            database = new Database(3);
+            int[] data = new int[3];
+            database = new Database(data);
             int expectedCount = database.Count + 1;
 
             database.Add(1);
@@ -46,12 +48,58 @@ namespace DatabaseTests
         [Fact]
         public void TryToAdd17ThElementToDatabase()
         {
+            int[] data = new int[16];
+            database = new Database(data);
             string expectedMessage = "Array's capacity must be exactly 16 integers!";
 
             Action act = () => database.Add(1);
 
             InvalidOperationException exception = Assert.Throws<InvalidOperationException>(act);
             Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Fact]
+        public void RemoveTheLastElementFromDatabase()
+        {
+            int expectedCount = database.Count - 1;
+
+            database.Remove();
+
+            Assert.Equal(expectedCount, database.Count);
+        }
+
+        [Fact]
+        public void TryToRemoveElementFromEmptyDatabase()
+        {
+            int[] data = new int[0];
+            database = new Database(data);
+            string expectedMessage = "The collection is empty!";
+
+            Action act = () => database.Remove();
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(act);
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Fact]
+        public void TakeIntOnlyAndStoreInArray()
+        {
+            int lengthOfArray = this.data.Length;
+            var typeOfInt = typeof(int);
+
+            for (int i = 0; i < lengthOfArray; i++)
+            {
+                var typeOfArray = this.data[i].GetType();
+                Assert.Equal(typeOfInt, typeOfArray);
+            }
+        }
+
+        [Fact]
+        public void FetchShouldReturnElementsAsArray()
+        {
+            int[] result = this.database.Fetch();
+
+            Assert.Equal(this.data, result);
         }
     }
 }
