0d73fd9e75ffe0e53ab12096e52aa306f02b7054
imp111 <tiso@DESKTOP-9D4P6IA.localdomain>
2023-08-26 20:14:37 +0300

    02. Sets of Elements - 25/100, not finished

---
+++ 04. Advanced/06. Sets and Dictionaries - Exercise/02. Sets of Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int n = input[0];
+int number = 0;
+List<int> firstList = new List<int>();
+for (int i = 0; i < input[0]; i++)
+{
+    number = int.Parse(Console.ReadLine());
+    firstList.Add(number);
+}
+
+int m = input[1];
+List<int> secondList = new List<int>();
+for (int i = 0;i < m; i++)
+{
+    number = int.Parse(Console.ReadLine());
+    secondList.Add(number);
+}
+
+List<int> intersectList = new List<int>();
+foreach (var item in firstList)
+{
+    if (secondList.Contains(item))
+    {
+        intersectList.Add(item);
+    }
+}
+
+for (int i = 0; i < intersectList.Count; i++)
+{
+    Console.Write(intersectList[i] + " ");
+
+    if (i == intersectList.Count - 1)
+    {
+        break;
+    }
+}
