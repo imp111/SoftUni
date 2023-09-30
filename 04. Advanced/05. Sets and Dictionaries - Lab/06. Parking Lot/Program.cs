4454ccce6a2d82e01281b463a75cbefb525e9858
imp111 <alexsandartenev@gmail.com>
2023-10-01 00:16:07 +0300

    06. Parking Lot - added and done (100/100)

--- 04. Advanced/05. Sets and Dictionaries - Lab/06. Parking Lot/Program.cs
+++ 04. Advanced/05. Sets and Dictionaries - Lab/06. Parking Lot/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = Console.ReadLine();
+
+HashSet<string> parkingLot = new HashSet<string>();
+while (input != "end")
+{
+    string[] car = input.Split(", ").ToArray();
+    string command = " ";
+    string number = " ";
+
+    if (car.Length < 2)
+    {
+        break;
+    }
+    else
+    {
+        command = car[0];
+        number = car[1];
+    }
+
+    if (command.ToLower() == "in")
+    {
+        parkingLot.Add(number);
+    }
+    else if (command.ToLower() == "out")
+    {
+        parkingLot.Remove(number);
+    }
+
+    input = Console.ReadLine();
+}
+
+if (parkingLot.Count == 0)
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
+else
+{
+    Console.WriteLine(string.Join("\n", parkingLot));
+}
+
