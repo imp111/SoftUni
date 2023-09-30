4e5f7c154676e322c54d996819068ccb74d09e4d
imp111 <alexsandartenev@gmail.com>
2023-09-30 23:55:52 +0300

    05. Record Unique Names - added and done (100/100)

--- 04. Advanced/05. Sets and Dictionaries - Lab/05. Record Unique Names/Program.cs
+++ 04. Advanced/05. Sets and Dictionaries - Lab/05. Record Unique Names/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine());
+
+HashSet<string> set = new HashSet<string>(n);
+
+for (int i = 0; i < n; i++)
+{
+    string name = Console.ReadLine();
+    set.Add(name);
+}
+
+Console.WriteLine(string.Join("\n", set));
