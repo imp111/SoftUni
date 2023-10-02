e6567ae39fa7e3046a3afe5e027a00b8796e8925
imp111 <alexsandartenev@gmail.com>
2023-10-02 13:19:49 +0300

    07. SoftUni Party - added and done (100/100)

--- 04. Advanced/05. Sets and Dictionaries - Lab/07. SoftUni Party/Program.cs
+++ 04. Advanced/05. Sets and Dictionaries - Lab/07. SoftUni Party/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string command = Console.ReadLine();
+
+HashSet<string> vipGuests = new HashSet<string>();
+HashSet<string> normalGuests = new HashSet<string>();
+
+while (command.ToLower() != "party")
+{
+    if (command.Count() == 8)
+    {
+        if (Char.IsDigit(command[0]))
+        {
+            vipGuests.Add(command);
+        }
+        else
+        {
+            normalGuests.Add(command);
+        }
+    }
+
+    command = Console.ReadLine();
+}
+
+command = Console.ReadLine();
+while (command.ToLower() != "end")
+{
+    if (vipGuests.Contains(command))
+    {
+        vipGuests.Remove(command);
+    }
+    else if (normalGuests.Contains(command))
+    {
+        normalGuests.Remove(command);
+    }
+    else
+    {
+        continue;
+    }
+
+    command = Console.ReadLine();
+}
+
+Console.WriteLine($"{vipGuests.Count + normalGuests.Count}");
+
+if (vipGuests.Count <= 0)
+{
+    Console.WriteLine(string.Join("\n", normalGuests));
+}
+else if (normalGuests.Count <= 0)
+{
+    Console.WriteLine(string.Join("\n", vipGuests));
+}
+else
+{
+    Console.WriteLine(string.Join("\n", vipGuests));
+    Console.WriteLine(string.Join("\n", normalGuests));
+}
