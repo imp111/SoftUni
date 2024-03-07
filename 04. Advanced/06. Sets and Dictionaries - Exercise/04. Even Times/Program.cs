940b3d988a691d0efd5ca0000b6c0e48fcd3df0f
imp111 <alexsandartenev@gmail.com>
2024-03-07 23:25:41 +0200

    04. Even Times - added and done (100/100)

---
+++ 04. Advanced/06. Sets and Dictionaries - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberOfIntegers = int.Parse(Console.ReadLine());
+
+Dictionary<int, int> numbers = new Dictionary<int, int>();
+for (int i = 0; i < numberOfIntegers; i++)
+{
+    var number = int.Parse(Console.ReadLine());
+    if (numbers.ContainsKey(number))
+    {
+        numbers[number]++;
+    }
+    else
+    {
+        numbers.Add(number, 1);
+    }
+}
+
+int evenTimesNumber = numbers.First(kvp => kvp.Value % 2 == 0).Key;
+Console.WriteLine(evenTimesNumber);
