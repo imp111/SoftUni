3ad5418e5eda928c619854586fba7ef628a44b01
imp111 <alexsandartenev@gmail.com>
2024-03-09 14:13:37 +0200

    05. Count Symbols - added and done (100/100)

---
+++ 04. Advanced/06. Sets and Dictionaries - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,19 @@
+﻿string input = Console.ReadLine();
+SortedDictionary<char, int> occurances = new SortedDictionary<char, int>();
+
+foreach (var item in input)
+{
+    if (occurances.ContainsKey(item))
+    {
+        occurances[item]++;
+    }
+    else
+    {
+        occurances.Add(item, 1);
+    }
+}
+
+foreach (var item in occurances)
+{
+    Console.WriteLine($"{item.Key} : {item.Value} time/s");
+}
