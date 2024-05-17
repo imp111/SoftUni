67c0848c0080940dd333485ded187aa2ac557812
imp111 <alexsandartenev@gmail.com>
2024-05-17 18:13:49 +0300

    recursion and backtracking added

---
+++ C# Development/05. Algorithms Fundamentals/01. Recursive Array Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿static int GetSum(int[] array, int index)
+{
+    if (index >= array.Length)
+    {
+        return 0;
+    }
+
+    return array[index] + GetSum(array, index + 1);
+}
+
+int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+Console.WriteLine(GetSum(input, 0));
